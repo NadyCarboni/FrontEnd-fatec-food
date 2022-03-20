3d189f975fe3ed93d5e2d7aae461cf767d408026
NadyCarboni <nady.n.m.c@gmail.com>
2022-03-20 16:16:25 -0300

    Estrutura inicial

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
