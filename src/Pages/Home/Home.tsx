975cf78725b0ea351821fbbc3a55dd66919afa46
NadyCarboni <nady.n.m.c@gmail.com>
2022-04-03 20:54:40 -0300

    Condicional de loading e animação

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Loading from "./Loading/Loading";
+import Main from "./Main/Main";
 
 function Home() {
-  return <Loading />;
+  const [loading, setLoading] = useState(true);
+
+  return <div>{loading && <Loading />}</div>;
 }
 
 export default Home;
--- src/Pages/Home/Loading/Componentes/Loader.tsx
+++ src/Pages/Home/Loading/Componentes/Loader.tsx
@@ -1,9 +1,23 @@
+import { motion } from "framer-motion";
+
 export default function Loader() {
   return (
     <div className="loader flex align-itens-center justify-content-center">
-      <div className="dot " />
-      <div className="dot mx-1" />
-      <div className="dot" />
+      <motion.div
+        className="dot "
+        animate={{ y: [0, -10, 0] }}
+        transition={{ repeat: Infinity }}
+      />
+      <motion.div
+        className="dot mx-1"
+        animate={{ y: [-10, 0, -10] }}
+        transition={{ repeat: Infinity }}
+      />
+      <motion.div
+        className="dot"
+        animate={{ y: [0, -10, 0] }}
+        transition={{ repeat: Infinity }}
+      />
     </div>
   );
 }
